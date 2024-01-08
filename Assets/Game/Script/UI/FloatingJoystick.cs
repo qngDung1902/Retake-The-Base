20d8da1b433ad22cc1edcb432cbdcffddd90d48b
Dunqnq <ngnquangdung1999@gmail.com>
2024-01-09 00:37:20 +0700

    Update

---
+++ Assets/Game/Script/UI/FloatingJoystick.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+[DisallowMultipleComponent]
+public class FloatingJoystick : MonoBehaviour
+{
+    public RectTransform RectTransform;
+    public RectTransform Knob;
+}
